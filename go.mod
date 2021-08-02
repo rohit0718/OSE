b8bbb07e0e11c20d914da84fc7f660fc8b1a18c8
rohit <rohit.rajesh@kaligo.com>
2021-08-02 14:35:19 +0800

    added logger

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rohit0718/OSE
 go 1.16
 
 require (
+	github.com/comail/colog v0.0.0-20160416085026-fba8e7b1f46c
 	github.com/julienschmidt/httprouter v1.3.0
 	k8s.io/kube-scheduler v0.21.3
 )
--- main.go
+++ main.go
@@ -4,13 +4,26 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/comail/colog"
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
+	colog.SetDefaultLevel(colog.LInfo)
+	colog.SetMinLevel(colog.LInfo)
+	colog.SetFormatter(&colog.StdFormatter{
+		Colors: true,
+		Flag:   log.Ldate | log.Ltime | log.Lshortfile,
+	})
+	colog.Register()
+	colog.SetMinLevel(colog.LTrace)
+
 	router := httprouter.New()
 	router.POST("/filter", Filter)
 	router.POST("/prioritize", Prioritize)
 
-	log.Fatal(http.ListenAndServe(":8888", router))
+	log.Print("info: server starting on the port :8888")
+	if err := http.ListenAndServe(":8888", router); err != nil {
+		log.Fatal(err)
+	}
 }
